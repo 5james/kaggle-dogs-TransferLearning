04059ac89e8c4d880d3bc5be8b1a0d22c48f01a6
Jakub Guzek <Jakub.K.Guzek@gmail.com>
2019-06-05 22:12:29 +0200

    Update statistics Add ROC

--- src/1/finetuning.py
+++ src/1/finetuning.py
@@ -13,13 +13,19 @@ import numpy as np
 from torchvision import datasets, models, transforms
 from tensorboardX import SummaryWriter
 import sys
-sys.path.append("..")
+import matplotlib
 from eval import *
 import copy
 import pickle
+import random
 import re
+import torchnet
+import plotly
+import plotly.graph_objs as go
 
+sys.path.append("..")
 
+np.set_printoptions(threshold=sys.maxsize)
 
 DATA_DIR = '../data/'
 LOG_DIR = 'logs/'
@@ -33,9 +39,12 @@ IMAGE_INPUT_SIZE = 224
 
 TRAIN_PART = 0.8
 VALIDATION_PART = 0.1
-# TEST_PART = 0.1
+TEST_PART = 0.1
 
 parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
+parser.add_argument("-e", "--experiment-dir", dest="EXPERIMENT_DIR", help="Pack whole experiment in one directory"
+                                                                          "with predeclared filenames",
+                    metavar="FILE", type=str, default=None)
 parser.add_argument("--batch_size", dest="BATCH_SIZE", help="DataLoader batch size",
                     type=int, default=32)
 parser.add_argument("--num_workers", dest="NUM_WORKERS", help="DataLoader number of workers",
@@ -43,105 +52,128 @@ parser.add_argument("--num_workers", dest="NUM_WORKERS", help="DataLoader number
 parser.add_argument("--epochs", dest="EPOCHS", help="number of epochs",
                     type=int, default=30)
 parser.add_argument("--learning_rate", dest="LEARNING_RATE", help="learning rate",
-                    type=float, default=0.001)
+                    type=float, default=0.04)
 parser.add_argument("--weight_decay", dest="WEIGHT_DECAY", help="weight decay (L2)",
                     type=float, default=0)
 parser.add_argument("--momentum", dest="MOMENTUM", help="momentum for optimizers",
                     type=float, default=0.9)
-parser.add_argument("--nogpu", dest="NOGPU", help="Indicates not to use GPU", action='store_true', default=False)
+parser.add_argument("--step_size", dest="STEP_SIZE", help="step size (step LR)",
+                    type=int, default=25)
+parser.add_argument("--gamma", dest="GAMMA", help="gamma (step LR)",
+                    type=float, default=0.1)
+parser.add_argument("--use_scheduler", dest="USE_SCHEDULER", help="use scheduler for optimizer",
+                    action='store_true')
+parser.add_argument("--nogpu", dest="NOGPU", help="Specify if you don't want to use GPU (CUDA)",
+                    action='store_true')
 
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-    since = time.time()
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
+    confusion_matrix = torchnet.meter.ConfusionMeter(NUM_CLASSES)
+    accuracy_meter = torchnet.meter.ClassErrorMeter(topk=[1, 5], accuracy=True)
+    total_loss_meter = 0
+    auc_meter_list = [torchnet.meter.AUCMeter() for _ in range(NUM_CLASSES)]
+    auc_avg = torchnet.meter.AUCMeter()
 
     for epoch in range(num_epochs):
         logger.info('-' * 60)
-        logger.info('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        logger.info('Epoch {}/{}'.format(epoch + 1, num_epochs))
         logger.info('-' * 10)
 
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
-            if phase == 'train':
-                scheduler.step()
-                model.train(True)  # Set model to training mode
-            else:
-                model.train(False)  # Set model to evaluate mode
+            confusion_matrix.reset()
+            accuracy_meter.reset()
+            total_loss_meter = 0
+            for auc_meter in auc_meter_list:
+                auc_meter.reset()
+            auc_avg.reset()
 
-            running_loss = 0.0
-            running_corrects_top1 = 0
-            running_corrects_top5 = 0
+            if phase == 'train':
+                if args.USE_SCHEDULER:
+                    scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
 
             # Iterate over data.
             for data in data_loaders[phase]:
                 # get the inputs
                 inputs, labels = data
-
-                # wrap them in Variable
-                if torch.__version__ == '0.3.1b0+4cf3225':
-                    if use_gpu:
-                        inputs = Variable(inputs.cuda())
-                        labels = Variable(labels.cuda())
-                    else:
-                        inputs, labels = Variable(inputs), Variable(labels)
-                else:
-                    inputs = inputs.to(device)
-                    labels = labels.to(device)
+                inputs = inputs.to(device)
+                labels = labels.to(device)
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
 
                 # forward
-                outputs = model(inputs)
-                xxx, preds = torch.max(outputs.data, 1)
-                loss = criterion(outputs, labels)
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs.data, 1)
+                    loss = criterion(outputs, labels)
 
-                # backward + optimize only if in training phase
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
 
                 # statistics
-                if torch.__version__ == '0.3.1b0+4cf3225':
-                    running_loss += float(loss.data[0]) * float(inputs.size(0))
-                    # print(float(loss.data[0]), float(inputs.size(0)),
-                    #       float(float(loss.data[0]) * float(inputs.size(0))), running_loss)
-                else:
-                    running_loss += float(loss.item()) * float(inputs.size(0))
-                # running_corrects_top1 += torch.sum(preds == labels.data)
-                top1, top5 = correct_count(outputs, labels, topk=(1, 5))
-                running_corrects_top1 += int(top1)
-                running_corrects_top5 += int(top5)
+                # basic
+                total_loss_meter += float(loss.item()) * float(inputs.size(0))
+                confusion_matrix.add(outputs.data.squeeze(), labels.type(torch.LongTensor))
+                accuracy_meter.add(outputs.data, labels.data)
+                # auc meter
+                for ii in range(NUM_CLASSES):
+                    targets = []
+                    for jj in labels.data:
+                        targets.append(1 if int(jj) == ii else 0)
+                    auc_meter_list[ii].add(outputs.data[:, ii], np.array(targets))
+                    auc_avg.add(outputs.data[:, ii], np.array(targets))
 
             logger.info('{}'.format(phase))
-            epoch_loss = running_loss / datasets_len[phase]
-            logger.info('Epoch Loss = {:6.4f}'.format(running_loss))
-            epoch_acc_top1 = int(running_corrects_top1) / datasets_len[phase]
-            logger.info('Epoch Accuracy Top1 = {:6.4f}'.format(epoch_acc_top1))
-            epoch_acc_top5 = int(running_corrects_top5) / datasets_len[phase]
-            logger.info('Epoch Accuracy Top5 = {:6.4f}'.format(epoch_acc_top5))
+            epoch_loss = total_loss_meter / datasets_len[phase]
+            logger.info('Epoch Loss / Dataset Len = {:6.4f}'.format(epoch_loss))
+            logger.info('Epoch Accuracy Top1 = {:6.4f}'.format(accuracy_meter.value(k=1)))
+            logger.info('Epoch Accuracy Top5 = {:6.4f}'.format(accuracy_meter.value(k=5)))
 
             if phase == 'train':
                 writer.add_scalar('Train/Loss', epoch_loss, epoch)
-                writer.add_scalar('Train/Accuracy-top1', epoch_acc_top1, epoch)
-                writer.add_scalar('Train/Accuracy-top5', epoch_acc_top5, epoch)
+                writer.add_scalar('Train/Accuracy-top1', accuracy_meter.value(k=1), epoch)
+                writer.add_scalar('Train/Accuracy-top5', accuracy_meter.value(k=5), epoch)
             elif phase == 'val':
                 writer.add_scalar('Val/Loss', epoch_loss, epoch)
-                writer.add_scalar('Val/Accuracy-top1', epoch_acc_top1, epoch)
-                writer.add_scalar('Val/Accuracy-top5', epoch_acc_top5, epoch)
+                writer.add_scalar('Val/Accuracy-top1', accuracy_meter.value(k=1), epoch)
+                writer.add_scalar('Val/Accuracy-top5', accuracy_meter.value(k=5), epoch)
 
-            # deep copy the model
-            if phase == 'val' and epoch_acc_top1 > best_acc:
-                best_acc = epoch_acc_top1
-                best_model_wts = copy.deepcopy(model.state_dict())
+            # ROC curve
+            mid_lane = go.Scatter(x=[0, 1], y=[0, 1],
+                                  mode='lines',
+                                  line=dict(color='navy', width=2, dash='dash'),
+                                  showlegend=False)
+            auc, tpr, fpr = auc_avg.value()
+            avg_lane = go.Scatter(x=fpr, y=tpr,
+                                  mode='lines',
+                                  line=dict(color='deeppink', width=1, dash='dot'),
+                                  name='average ROC curve (area = {0:0.2f})'.format(auc))
+            traces = [mid_lane, avg_lane]
+            for ii in range(NUM_CLASSES):
+                auc, tpr, fpr = auc_meter_list[ii].value()
+                color = 'rgb({}, {}, {})'.format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                trace = go.Scatter(x=fpr, y=tpr,
+                                   mode='lines',
+                                   line=dict(color=color, width=1),
+                                   name='{} (area = {0.2f})'.format(idx_to_class[ii], auc)
+                                   )
+                traces.append(trace)
+            layout = go.Layout(title='Receiver operating characteristic',
+                               xaxis=dict(title='False Positive Rate'),
+                               yaxis=dict(title='True Positive Rate'))
+            plotly.offline.plot({
+                "data": traces,
+                "layout": layout
+            }, auto_open=True)
 
-    time_elapsed = time.time() - since
-    logger.info('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-    logger.info('Best val Acc: {:4f}'.format(best_acc))
-
-    # load best model weights
-    model.load_state_dict(best_model_wts)
+    # # load best model weights
+    # model.load_state_dict(best_model_wts)
     return model
 
 
@@ -149,16 +181,27 @@ if __name__ == "__main__":
     # parse args
     args = parser.parse_args()
 
+    EXPERIMENT_DIR = args.EXPERIMENT_DIR
+    if EXPERIMENT_DIR is not None:
+        if EXPERIMENT_DIR[-1] is not '/':
+            EXPERIMENT_DIR += '/'
+        if not os.path.isdir(EXPERIMENT_DIR):
+            os.mkdir(EXPERIMENT_DIR)
+        else:
+            raise Exception('Experiment with name {} already exists'.format(EXPERIMENT_DIR))
+    else:
+        raise Exception('No experiment directory given.')
+
     # create logger
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
     # make sure logging directory 'logs' is available
-    if not os.path.isdir(LOG_DIR):
-        os.mkdir(LOG_DIR)
+    if not os.path.isdir(EXPERIMENT_DIR + LOG_DIR):
+        os.mkdir(EXPERIMENT_DIR + LOG_DIR)
 
     # create file handler which logs messages
-    fh = logging.FileHandler(LOG_DIR + str(os.path.basename(__file__).split('.')[0]) + '.log')
+    fh = logging.FileHandler(EXPERIMENT_DIR + LOG_DIR + str(os.path.basename(__file__).split('.')[0]) + '.log')
     fh.setLevel(logging.DEBUG)
 
     # create console handler to print to screen
@@ -176,31 +219,28 @@ if __name__ == "__main__":
     logger.info(args)
 
     # tensorboardX logger
-    tensorboard_dir = LOG_DIR + 'tensorboard'
+    tensorboard_dir = EXPERIMENT_DIR + LOG_DIR + 'tensorboard'
     if not os.path.isdir(tensorboard_dir):
         os.mkdir(tensorboard_dir)
     writer = SummaryWriter(tensorboard_dir)
 
-    # FREEZE_LAYERS_NUMBER = 1
-    #
-    #
-    # def freeze_params(parameters):
-    #     model_layers_len = 0
-    #     for para in parameters:
-    #         model_layers_len += 1
-    #         para.requires_grad = False
-    #     freeze_idx = 0
-    #     for para in parameters:
-    #         freeze_idx += 1
-    #         if freeze_idx >= model_layers_len - FREEZE_LAYERS_NUMBER:
-    #             para.requires_grad = True
+    FREEZE_LAYERS_NUMBER = 1
+
+
+    def freeze_params(parameters):
+        model_layers_len = 0
+        for para in parameters:
+            model_layers_len += 1
+            para.requires_grad = False
+        freeze_idx = 0
+        for para in parameters:
+            freeze_idx += 1
+            if freeze_idx >= model_layers_len - FREEZE_LAYERS_NUMBER:
+                para.requires_grad = True
 
 
     # create model
-    model_ft = models.vgg16(pretrained=True)
-    # freeze all layers
-    for para in model_ft.parameters():
-        para.requires_grad = False
+    model_ft = models.vgg19_bn(pretrained=True)
     # newly created layers have requires_grad == True
     model_ft.classifier = nn.Sequential(
         nn.Dropout(p=0.5),
@@ -249,27 +289,37 @@ if __name__ == "__main__":
 
     data_loaders = {'train': train_dataloader, 'val': val_dataloader, 'test': test_dataloader}
 
+    # Create map index -> class name
+    idx_to_class = res = dict((v, k) for k, v in dataset_all.class_to_idx.items())
+    logger.info(idx_to_class)
+    # Get rid of rubbish in class name
+    for i in idx_to_class:
+        search_re = re.search(r'(n\d+-)(\w+)', idx_to_class[i], re.I)
+        if search_re is not None:
+            idx_to_class[i] = search_re.group(2)
+    logger.info(idx_to_class)
+
     # check if can use GPU
-    if torch.__version__ == '0.3.1b0+4cf3225':
-        use_gpu = torch.cuda.is_available() and not args.NOGPU
-        if use_gpu:
-            model_ft = model_ft.cuda()
-    else:
-        device = torch.device("cuda" if torch.cuda.is_available() and not args.NOGPU else "cpu")
-        model_ft = model_ft.to(device)
+    device = torch.device("cuda" if torch.cuda.is_available() and not args.NOGPU else "cpu")
+    model_ft = model_ft.to(device)
 
     criterion = nn.CrossEntropyLoss()
 
-    logger.info(
-        'SGD: lr = {};  momentum = {}, weight decay = {}'.format(args.LEARNING_RATE, args.MOMENTUM, args.WEIGHT_DECAY))
-    optimizer_ft = optim.SGD(filter(lambda p: p.requires_grad, model_ft.parameters()),
-                             lr=args.LEARNING_RATE, momentum=args.MOMENTUM, weight_decay=args.WEIGHT_DECAY)
+    params_to_update = []
+    for name, param in model_ft.named_parameters():
+        if param.requires_grad:
+            params_to_update.append(param)
+            logger.info("\t{}".format(name))
 
-    # Decay LR by a factor of 0.1 every 7 epochs
-    step_size = 7
-    gamma = 0.1
-    logger.info('StepLR: step_size = {};  gamma = {}'.format(step_size, gamma))
-    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=step_size, gamma=gamma)
+    logger.info('SGD: lr = {};  momentum = {}, weight decay = {}'.format(
+        args.LEARNING_RATE, args.MOMENTUM, args.WEIGHT_DECAY))
+
+    optimizer_ft = optim.SGD(params_to_update, lr=args.LEARNING_RATE, momentum=args.MOMENTUM,
+                             weight_decay=args.WEIGHT_DECAY)
+
+    # Decay LR by a factor of x every y epochs
+    logger.info('StepLR: step_size = {};  gamma = {}'.format(args.STEP_SIZE, args.GAMMA))
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=args.STEP_SIZE, gamma=args.GAMMA)
 
     model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
                            num_epochs=args.EPOCHS)
